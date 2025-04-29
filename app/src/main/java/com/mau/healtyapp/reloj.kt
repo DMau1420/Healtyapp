0d7b8308ea80fc61169c635b7f75a739fcc7a3f7
Figuras <shootingdarcklight@gmail.com>
2025-04-29 12:37:24 -0600

    pantalla de registro

---
+++ app/src/main/java/com/mau/healtyapp/reloj.kt
@@ -0,0 +1,11 @@
+package com.mau.healtyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reloj : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reloj)
+    }
+}
