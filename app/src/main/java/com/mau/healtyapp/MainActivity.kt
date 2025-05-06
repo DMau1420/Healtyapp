8b3961af51adff9220840e1d07f4e0b589d723e7
Figuras <shootingdarcklight@gmail.com>
2025-05-05 19:46:59 -0600

    login y registro funcionando con servidor

--- app/src/main/java/com/mau/healtyapp/MainActivity.kt
+++ app/src/main/java/com/mau/healtyapp/MainActivity.kt
@@ -1,11 +1,119 @@
 package com.mau.healtyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var registrar : TextView
+    private lateinit var email : EditText
+    private lateinit var password : EditText
+    private lateinit var verpass : ImageView
+    private lateinit var entrar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        email = findViewById(R.id.Email)
+        password = findViewById(R.id.Password)
+
+        registrar = findViewById(R.id.Registrarse)
+        registrar.setOnClickListener {
+            startActivity(Intent(this, Registro::class.java))
+        }//fin del boton registrar
+
+        verpass = findViewById(R.id.verpas)
+        verpass.setOnClickListener {
+            if (password.inputType == InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT) {
+                password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            } else {
+                password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+            password.setSelection(password.text.length)
+        }//fin de ocultar contraseña
+
+        entrar = findViewById(R.id.Entrar)
+
+        entrar.setOnClickListener {
+            val email = email.text.toString().trim()
+            val password = password.text.toString().trim()
+
+            if (validateInputs(email, password)) {
+                verificar(email, password) { success ->
+                    if (success) {
+                        val intent = Intent(this, reloj::class.java).apply {
+                            putExtra("email", email)
+                        }
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
+    private fun validateInputs(eemail: String, ppassword: String): Boolean {
+        var isValid = true
+
+        if (eemail.isEmpty()) {
+            email.error = "Ingrese su correo"
+            isValid = false
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(eemail).matches()) {
+            email.error = "Correo no válido"
+            isValid = false
+        }
+        if (ppassword.isEmpty()) {
+            password.error = "Ingrese su contraseña"
+            isValid = false
+        } else if (ppassword.length < 6) {
+            password.error = "Mínimo 6 caracteres"
+            isValid = false
+        }
+
+        return isValid
+    }//fin del metodo validar inputs
+
+    private fun verificar(email: String, password: String, callback: (Boolean) -> Unit) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                //cambiar url por la que se vaya  a ocupar y claramente hacer su php
+                val url = URL("http://162.243.81.73/login.php?correo=$email&contra=$password")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+                val response = connection.inputStream.bufferedReader().use { it.readText() }
+
+                val jsonObject = JSONObject(response)
+                val correo = jsonObject.getString("correo")
+                val contrasena = jsonObject.getString("contra")
+
+                runOnUiThread {
+                    callback(email == correo && password == contrasena)
+                }
+            }
+            catch (e: Exception) {
+                Log.e("LoginError", "Error de conexión", e)
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Error: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+                    callback(false)
+                }
+            }
+        }
+    }//fin del metodo verificar
+
 }
--- app/src/main/java/com/mau/healtyapp/Registro.kt
+++ app/src/main/java/com/mau/healtyapp/Registro.kt
@@ -1,11 +1,143 @@
 package com.mau.healtyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import java.net.HttpURLConnection
+import java.net.URL
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class Registro : AppCompatActivity() {
+    private lateinit var registrar : TextView
+    private lateinit var tipousuario : Spinner
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var confpass: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        //iniciospiner
+        tipousuario = findViewById(R.id.spinnerTipoUsuario)
+
+        val opciones = arrayOf("<Seleccionar>","Paciente", "Cuidador", "Independiente")
+        val adapter = object : ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones) {
+            override fun isEnabled(position: Int): Boolean {
+                return position != 0 // Desactiva la opción "<Seleccionar>"
+            }
+
+            override fun getDropDownView(position: Int, convertView: View?, parent: android.view.ViewGroup): View {
+                val view = super.getDropDownView(position, convertView, parent)
+                val textView = view as TextView
+                if (position == 0) {
+                    textView.setTextColor(android.graphics.Color.GRAY)
+                } else {
+                    textView.setTextColor(android.graphics.Color.BLACK)
+                }
+                return view
+            }
+        }
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        tipousuario.adapter = adapter
+        //fin spinner
+
+        email = findViewById(R.id.Email)
+        password = findViewById(R.id.Password)
+        confpass = findViewById(R.id.confPassword)
+
+        registrar = findViewById(R.id.Entrar)
+        registrar.setOnClickListener {
+            val a = email.text.toString().trim()
+            val b = password.text.toString().trim()
+            val c = confpass.text.toString().trim()
+            val d = tipousuario.selectedItem.toString().trim()
+
+            if (validateInputs(a,b,c,d)) {
+                val u = generarUID6()
+
+                registrarUser(a, b, u, d)
+                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+
+
     }
+    private fun validateInputs(eemail: String, ppassword: String, confirmPassword: String, tipous: String): Boolean {
+        var isValid = true
+        if (tipous == "<Seleccionar>"){
+            Toast.makeText(this, "Seleccione un tipo de usuario", Toast.LENGTH_SHORT).show()
+            isValid = false
+        }
+
+        if (eemail.isEmpty()) {
+            email.error = "Ingrese su correo"
+            isValid = false
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(eemail).matches()) {
+            email.error = "Correo no válido"
+            isValid = false
+        }
+
+        if (ppassword.isEmpty()) {
+            password.error = "Ingrese su contraseña"
+            isValid = false
+        } else if (ppassword.length < 6) {
+            password.error = "Mínimo 6 caracteres"
+            isValid = false
+        }
+
+        if (confirmPassword.isEmpty()) {
+            confpass.error = "Confirme su contraseña"
+            isValid = false
+        } else if (ppassword != confirmPassword) {
+            confpass.error = "Las contraseñas no coinciden"
+            isValid = false
+        }
+        return isValid
+    }//fin validar inputs
+
+    fun generarUID6(): String {
+        val caracteres = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        return (1..6)
+            .map { caracteres.random() }
+            .joinToString("")
+    }//creacion del UID (user id)
+
+    private fun registrarUser (correo: String, password: String, uid : String, tipo: String){
+    lifecycleScope.launch(Dispatchers.IO) {
+        try {
+            val url = URL("http://162.243.81.73/registro.php")
+            val params = "uid=$uid&correo=$correo&contra=$password&tipo=$tipo"
+
+            with(url.openConnection() as HttpURLConnection) {
+                requestMethod = "POST"
+                doOutput = true
+                outputStream.write(params.toByteArray())
+
+                val response = inputStream.bufferedReader().use { it.readText() }
+
+                runOnUiThread {
+                    Toast.makeText(this@Registro, response, Toast.LENGTH_LONG).show()
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            runOnUiThread {
+                Toast.makeText(this@Registro, "Error: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
 }
